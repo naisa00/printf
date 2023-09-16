558365eccd1edcc3c0d6f81df9a48549b5c76c4c
mariagirges <139780944+mariagirges@users.noreply.github.com>
2023-09-16 17:34:49 +0300

    1. Education is when you read the fine print. Experience is what you get if you don't

--- get_flags.c
+++ get_flags.c
@@ -5,11 +5,11 @@
  * @i: take a parameter
  * Return: flags
  */
-int get_flags(cons char *format, int *i)
+int get_flags(const char *format, int *i)
 {
 	int d, n;
 	int flags = 0;
-	const char FLAGS_CH[] = {'-', '+', '0', '#', '', '\0'};
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
 	for (n = *i + 1; format[n] != '\0'; n++)
